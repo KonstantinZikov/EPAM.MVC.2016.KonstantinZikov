81ebc67b81b4d77f9e3e7cff295074571c858290
Kanstantsin Zykau <Kanstantsin_Zykau@epam.com>
2016-08-31 11:31:47 +0300

    project loaded

---
+++ Task 1/WebApplication1/JsonService/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace JsonService.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ActionResult Index(string data)
+        {
+            data = data ?? "null";
+            var result = new JsonResult();
+            result.Data = "Json: " + data;
+            return result;
+        }
+    }
+}
