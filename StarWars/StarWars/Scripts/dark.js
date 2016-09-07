d62dc2150e5dffc3afea79af94ba5f06022e820b
Kanstantsin Zykau <Kanstantsin_Zykau@epam.com>
2016-09-07 12:20:06 +0300

    task done, but url routing for images and scripts doesn't work correctly

---
+++ StarWars/StarWars/Scripts/dark.js
@@ -0,0 +1,10 @@
+﻿function changeSideNo() {
+    var $footer = $(".footer");
+    $($footer.children()).remove();
+    $footer.text("Cool choose BRO!");
+}
+function changeSideYes() {
+    var $footer = $(".footer");
+    $($footer.children()).remove();
+    $footer.text("Prepare uranus.");
+}
---
+++ StarWars/StarWars/Scripts/light.js
@@ -0,0 +1,11 @@
+﻿function changeSideNo() {
+    var $footer = $(".footer");
+    $($footer.children()).remove();
+    $footer.text("Cool choose BRO!");
+}
+function changeSideYes() {
+    var id = $(".content").attr("data-id");
+    $.post("Home/ChangeSide/" + id, {}, function () {
+        location.reload();
+    })
+}
